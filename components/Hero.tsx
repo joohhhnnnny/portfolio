1433f325383a8f96871238d25c4cc46beaa412c3
John Benedict Bongcac <johnbongcacjohn@gmail.com>
2026-01-04 16:00:44 +0800

    hero section started

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,36 @@
+import { Spotlight } from '@/components/ui/spotlight'
+import { cn } from '@/lib/utils'
 import React from 'react'
 
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-        <div>
+      <div>
+        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white"/>
+        <Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="purple"/>
+        <Spotlight className="h-[80vh] w-[50vw] top-28 left-80" fill="blue"/>
+      </div>
 
+      <div className="flex h-screen w-full items-center justify-center bg-white dark:bg-black-100 absolute top-0 left-0 ">
+        <div
+          className={cn(
+            "absolute inset-0",
+            "[background-size:40px_40px]",
+            "[background-image:linear-gradient(to_right,#e4e4e720_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e720_1px,transparent_1px)]",
+            "dark:[background-image:linear-gradient(to_right,#26262620_1px,transparent_1px),linear-gradient(to_bottom,#26262620_1px,transparent_1px)]",
+          )}
+        />
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
+      </div>
+
+      <div className="flex justify-center relative my-20 z-10">
+        <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+            Dynamic Web Magic with Next.js
+          </h2>
         </div>
+      </div>
+        
     </div>
   )
 }
